db260638e1e4aff65a8d5f8c8e76c3ecae67641b
Ewan Gravé <grave.ewan2001@gmail.com>
2024-11-30 18:33:14 +0100

    refacto: remove unused routes

--- latrium-back/src/main/java/com/example/latriumback/controller/UserController.java
+++ latrium-back/src/main/java/com/example/latriumback/controller/UserController.java
@@ -7,17 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
-
-    @GetMapping("/api/users")
-    public List<UserDTO> getAllUsers() {
-        return userService.findAll();
-    }
 
     @GetMapping("/api/users/{name}")
     public UserDTO getUserByUsername(@PathVariable String name)  {
--- latrium-back/src/main/java/com/example/latriumback/service/UserService.java
+++ latrium-back/src/main/java/com/example/latriumback/service/UserService.java
@@ -6,9 +6,6 @@ import com.example.latriumback.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 public class UserService {
     @Autowired
@@ -17,11 +14,6 @@ public class UserService {
     public UserDTO findByUsername(String username) {
         User user = userRepository.findByUsername(username);
         return convertToDTO(user);
-    }
-
-    public List<UserDTO> findAll() {
-        List<User> users = userRepository.findAll();
-        return users.stream().map(this::convertToDTO).collect(Collectors.toList());
     }
 
     private UserDTO convertToDTO(User user) {

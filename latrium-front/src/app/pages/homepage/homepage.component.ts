d602733e47bf13c1a97063d00f631a6bfe36bff1
Ewan Gravé <grave.ewan2001@gmail.com>
2024-12-02 17:31:45 +0100

    feat(): generate client from swagger spec

--- latrium-front/src/app/pages/homepage/homepage.component.ts
+++ latrium-front/src/app/pages/homepage/homepage.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { PostitemComponent } from '../../components/postitem/postitem.component';
-import { PostService } from '../../servives/postservice.service';
 
 @Component({
   selector: 'app-homepage',
@@ -9,11 +8,4 @@ import { PostService } from '../../servives/postservice.service';
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
-export class HomepageComponent implements OnInit {
-  private postService = inject(PostService);
-  posts = this.postService.posts;
-
-  ngOnInit() {
-    this.postService.getAllPosts().subscribe();
-  }
-}
+export class HomepageComponent {}

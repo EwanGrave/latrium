b2687cc543df2696c2763d9d4218ca81b3f381af
Ewan Gravé <grave.ewan2001@gmail.com>
2024-12-07 15:30:53 +0100

    feat(): get themes from api

--- latrium-front/src/app/components/sidebar/sidebar.component.ts
+++ latrium-front/src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ThemeControllerService, ThemeDTO } from '../../../../api';
 
 @Component({
   selector: 'app-sidebar',
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  themeService = inject(ThemeControllerService);
+  themes!: ThemeDTO[];
 
+  ngOnInit(): void {
+    this.themeService.getAllThemes().subscribe({
+      next: (themes) => {
+        this.themes = themes;
+      },
+    });
+  }
 }

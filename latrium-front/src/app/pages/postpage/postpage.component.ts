4bf5067ccbf653fe2599512f26d723dad973ab99
Ewan Gravé <grave.ewan2001@gmail.com>
2024-12-19 14:00:42 +0100

    feat: post page style

--- latrium-front/src/app/pages/postpage/postpage.component.ts
+++ latrium-front/src/app/pages/postpage/postpage.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostControllerService, PostDTO } from '../../../../api';
+import { formatDateFromString } from '../../utils/StringUtils';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-postpage',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './postpage.component.html',
-  styleUrl: './postpage.component.css'
+  styleUrl: './postpage.component.css',
 })
-export class PostpageComponent {
+export class PostpageComponent implements OnInit {
+  route = inject(ActivatedRoute);
+  postService = inject(PostControllerService);
+  idPost!: number;
+  post!: PostDTO | undefined;
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.idPost = params['idPost'];
+    });
+
+    this.postService.getPostById(this.idPost).subscribe((value) => {
+      this.post = value;
+      this.post.createdAt = formatDateFromString(this.post.createdAt ?? '');
+      this.post.comments?.forEach((comment) => {
+        comment.createdAt = formatDateFromString(comment.createdAt ?? '');
+      });
+    });
+  }
 }
